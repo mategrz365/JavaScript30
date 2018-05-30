33650cba691c800c77cd6ee4cd76181577c21590
mategrz365 <mategrz365@gmail.com>
2018-05-30 09:47:41 +0200

    files updated
    bug in second-hand, to change a method of finding an angles.

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,32 +1,36 @@
-    let date = new Date();       
-    let seconds = date.getSeconds();
-    let minutes = date.getMinutes();
+   
     let secondHand = document.querySelector('.second-hand');
     let minuteHand = document.querySelector('.min-hand');
     let hourHand = document.querySelector('.hour-hand');
 
-    let FindHour = function(){
-        let hour = date.getHours(); 
+ function FindHour(hour){
         if (hour>=12) hour = hour - 12;
         return hour;
     } 
     
-    let RotateAngleSeconds = function(){
+  function RotateAngleSeconds(seconds) {
+      /*if (seconds == 0) seconds = 60;*/
         return 'rotate('+ seconds * 6 + 'deg' + ')';
     }
     
-    let RotateAngleHour = function(){
-        return 'rotate('+ FindHour() * 6 + 'deg' + ')';
+    function RotateAngleHour(hour) {
+        return 'rotate('+ hour * 30 + 'deg' + ')';
     }
     
-    let RotateAngleMinutes = function(){
-        return 'rotate('+ minutes * 30 + 'deg' + ')';
+  function RotateAngleMinutes (minutes) {
+        return 'rotate('+ minutes * 6 + 'deg' + ')';
     }
 
-    function setCurrentTime() {             
-            secondHand.style.webkitTransform = RotateAngleSeconds();
-            hourHand.style.webkitTransform = RotateAngleHour();
-            minuteHand.style.webkitTransform = RotateAngleMinutes();
+    function setCurrentTime() {  
+         let date = new Date();       
+         let seconds = date.getSeconds();
+         let minutes = date.getMinutes();
+         let hour = date.getHours(); 
+         FindHour(hour);
+          secondHand.style.webkitTransform = RotateAngleSeconds(seconds);
+            hourHand.style.webkitTransform = RotateAngleHour(hour);
+           minuteHand.style.webkitTransform = RotateAngleMinutes(minutes);
+           console.log(hour);            
     };
            
     setInterval(setCurrentTime, 1000);

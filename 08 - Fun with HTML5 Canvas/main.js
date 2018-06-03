eae801ff69fab739f20fb04bf6cde53880cd7271
mategrz365 <mategrz365@gmail.com>
2018-06-03 13:00:32 +0200

    HTML5 Canvas

---
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -0,0 +1,41 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+let lastX = 0;
+let lastY = 0;
+let isDrawing = false;
+let hue = 0;
+let direction = true;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function draw(e){
+        if (!isDrawing) return;
+        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+        hue++;
+
+            if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1)
+                direction = !direction;
+
+                if(direction){
+                    ctx.lineWidth++;
+                } else{
+                    ctx.lineWidth--;
+                }
+}
+
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+isDrawing = true;
+[lastX, lastY] = [e.offsetX, e.offsetY];   
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
